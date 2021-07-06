5dec1d43c88d16ada48e0b33f0a6d81cb1e94120
Michael Bonner <michaelwbonner@gmail.com>
2021-07-06 10:24:27 -0600

    little more styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,6 +21,7 @@
 	if (typeof window !== 'undefined' && !isValid(parse(selectedDay, 'yyyy-MM-dd', new Date()))) {
 		alert('Make sure you are passing dates as yyyy-MM-dd format');
 	}
+	$: selectedDateFormatted = format(selectedDate, 'MM/dd/yyyy');
 	$: daysUntilSelectedDate = new Intl.NumberFormat().format(
 		differenceInCalendarDays(selectedDate, today)
 	);
@@ -30,9 +31,10 @@
 </script>
 
 <div class="text-center">
-	<h1 class="text-2xl lg:text-3xl py-2">Welcome to Days Until</h1>
+	<h1 class="text-2xl lg:text-3xl py-2">Days Until</h1>
 	<p class="mt-4 max-w-sm mx-auto">
-		Enter your target date below and we'll tell you how many days away it is.
+		Enter your target date below and the display below will tell you how many days away your date
+		is. That&apos;s all there is it
 	</p>
 
 	<div class="lg:flex lg:flex-wrap justify-between items-center mt-12 mx-2 lg:mx-12">
@@ -48,14 +50,24 @@
 
 	<div class="lg:grid grid-cols-2 text-center border-t pt-12 mt-12">
 		<div>
-			<div>Days Until {selectedDay}</div>
-			<div class="text-3xl">{daysUntilSelectedDate}</div>
+			<div class="text-4xl">{daysUntilSelectedDate}</div>
+			<div class="text-lg">Days Until</div>
+			<div class="text-sm">{selectedDateFormatted}</div>
 		</div>
 		<div class="mt-4 lg:mt-0">
-			<div>Business Days Until {selectedDay}</div>
-			<div class="text-3xl">
+			<div class="text-4xl">
 				{businessDaysUntilSelectedDate}
 			</div>
+			<div class="text-lg">Business Days Until</div>
+			<div class="text-sm">{selectedDateFormatted}</div>
 		</div>
+	</div>
+	<div class="mt-12 max-w-md mx-auto">
+		<p class="text-center text-sm text-gray-800">
+			FYI: You can also give this page a date in the url. Something like
+			<a class="text-indigo-600 underline" href="https://days-until.vercel.app/?date=2050-01-01"
+				>https://days-until.vercel.app/?date=2050-01-01</a
+			>
+		</p>
 	</div>
 </div>
