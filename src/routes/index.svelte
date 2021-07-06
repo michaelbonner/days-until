2ff3d652cebbbca0a1d935f9cdadf911b3314baa
Michael Bonner <michaelwbonner@gmail.com>
2021-07-06 09:19:00 -0600

    handle time zones on inpute

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,21 @@
 <script>
-	import { differenceInBusinessDays, differenceInCalendarDays, format } from 'date-fns';
+	import { differenceInBusinessDays, differenceInCalendarDays } from 'date-fns';
+	import { format, utcToZonedTime } from 'date-fns-tz';
 	const today = new Date();
 	const todayFormatted = format(today, 'MM/dd/yyyy');
 	let selectedDay = todayFormatted;
-	$: selectedDate = new Date(selectedDay);
+	$: selectedDate = utcToZonedTime(new Date(selectedDay), new Date().getTimezoneOffset());
 	$: daysUntilSelectedDate = differenceInCalendarDays(selectedDate, today);
 	$: businessDaysUntilSelectedDate = differenceInBusinessDays(selectedDate, today);
 </script>
 
 <h1 class="font-light text-3xl py-2">Welcome to Days Until</h1>
 <p class="mt-4">Enter your target date below and we'll tell you how many days away it is.</p>
-<p class="mt-4">Today&apos;s date is {todayFormatted}</p>
+<p class="mt-4">
+	Today&apos;s date is <code class="bg-gray-100 py-1 px-2 font-sans rounded">
+		{todayFormatted}</code
+	>
+</p>
 
 <div class="mt-8">
 	<input bind:value={selectedDay} class="border py-2 px-4" type="date" />
@@ -18,11 +23,11 @@
 
 <div class="grid grid-cols-2 text-center border-t pt-12 mt-12">
 	<div>
-		<div>Days between</div>
+		<div>Days Until {selectedDay}</div>
 		<div class="text-3xl">{daysUntilSelectedDate}</div>
 	</div>
 	<div>
-		<div>Business days between</div>
+		<div>Business Days Until {selectedDay}</div>
 		<div class="text-3xl">
 			{businessDaysUntilSelectedDate}
 		</div>
