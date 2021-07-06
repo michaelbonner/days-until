e178a68768fe4dd54d71a7b7e2637404bb4c893e
Michael Bonner <michaelwbonner@gmail.com>
2021-07-06 10:16:16 -0600

    different full height

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,7 @@
 	import '../styles/tailwind-output.css';
 </script>
 
-<div class="min-h-screen flex flex-col justify-center items-center">
+<div class="ui-main-wrapper lg:min-h-screen flex flex-col justify-center items-center">
 	<div
 		class="mx-6 lg:mx-auto my-12 px-8 py-4 lg:px-8 lg:py-8 bg-white max-w-3xl rounded-lg shadow-lg"
 	>
