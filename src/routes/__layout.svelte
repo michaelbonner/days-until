c36fc763986c442cde48aecb176766bfc8c543b3
Michael Bonner <michaelwbonner@gmail.com>
2021-07-06 10:03:49 -0600

    mobile style

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,8 @@
 	import '../styles/tailwind-output.css';
 </script>
 
-<!-- Try some classes here -->
-<div class="mx-auto my-12 p-8 bg-white max-w-3xl rounded-lg shadow-lg">
-	<slot />
+<div class="min-h-screen flex flex-col justify-center items-center">
+	<div class="mx-6 lg:mx-auto my-12 p-8 bg-white max-w-3xl rounded-lg shadow-lg">
+		<slot />
+	</div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,29 +29,31 @@
 	);
 </script>
 
-<h1 class="text-3xl py-2">Welcome to Days Until</h1>
-<p class="mt-4">Enter your target date below and we'll tell you how many days away it is.</p>
+<div>
+	<h1 class="text-3xl py-2">Welcome to Days Until</h1>
+	<p class="mt-4">Enter your target date below and we'll tell you how many days away it is.</p>
 
-<div class="flex justify-between items-center mt-12 mx-2 lg:mx-12">
-	<div class="mx-auto">
-		<input bind:value={selectedDay} class="border py-2 px-4 bg-gray-50" type="date" />
+	<div class="lg:flex lg:flex-wrap justify-between items-center mt-12 mx-2 lg:mx-12">
+		<div class="mx-auto w-full lg:w-1/2 text-center">
+			<input bind:value={selectedDay} class="border py-2 px-4 bg-gray-50" type="date" />
+		</div>
+		<p class="mx-auto w-full lg:w-1/2 text-center mt-4 lg:mt-0">
+			Today&apos;s date is <code class="bg-gray-100 py-1 px-2 font-sans rounded">
+				{todayFormatted}</code
+			>
+		</p>
 	</div>
-	<p class="mx-auto">
-		Today&apos;s date is <code class="bg-gray-100 py-1 px-2 font-sans rounded">
-			{todayFormatted}</code
-		>
-	</p>
-</div>
 
-<div class="grid grid-cols-2 text-center border-t pt-12 mt-12">
-	<div>
-		<div>Days Until {selectedDay}</div>
-		<div class="text-3xl">{daysUntilSelectedDate}</div>
-	</div>
-	<div>
-		<div>Business Days Until {selectedDay}</div>
-		<div class="text-3xl">
-			{businessDaysUntilSelectedDate}
+	<div class="lg:grid grid-cols-2 text-center border-t pt-12 mt-12">
+		<div>
+			<div>Days Until {selectedDay}</div>
+			<div class="text-3xl">{daysUntilSelectedDate}</div>
+		</div>
+		<div class="mt-4 lg:mt-0">
+			<div>Business Days Until {selectedDay}</div>
+			<div class="text-3xl">
+				{businessDaysUntilSelectedDate}
+			</div>
 		</div>
 	</div>
 </div>
