2ddbbd8aa64b2bdcc6c4271ab6ae490d72001e09
Michael Bonner <mike@bootpackdigital.com>
2025-05-29 12:31:37 -0600

    Copy svelte starter app config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,9 @@
-import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: vercel()
-	}
+	preprocess: vitePreprocess(),
+	kit: { adapter: adapter() }
 };
 
 export default config;
