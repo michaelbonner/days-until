23ff64c4b4dd369bc2d547b302955d592ad2b94c
Michael Bonner <mike@bootpackdigital.com>
2025-05-29 21:49:01 -0600

    Tiny spacing update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,13 @@
 
 <div class="ui-main-wrapper flex flex-col items-center justify-center px-4 lg:min-h-screen lg:px-0">
 	<div
-		class="mx-6 my-4 w-full max-w-3xl rounded-xs bg-white px-6 py-4 shadow-lg lg:mx-auto lg:px-12 lg:py-8 xl:px-24 xl:py-12"
+		class="mx-6 my-4 w-full max-w-4xl rounded-lg bg-white px-6 py-4 shadow-lg lg:mx-auto lg:mt-12 lg:px-12 lg:py-8 xl:px-24 xl:py-12"
 	>
 		<slot />
 	</div>
 
-	<div class="prose">
-		<p class="mt-12 pb-8 text-center text-sm text-slate-800">
+	<div class="prose mt-4 mb-8">
+		<p class="text-center text-sm text-slate-800">
 			&copy;2021-{new Date().getFullYear()}
 			<a class="underline" href="https://michaelbonner.dev"> Michael Bonner </a>
 		</p>
