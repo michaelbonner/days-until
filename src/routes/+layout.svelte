7cea812dfc5a35bbc5657df05b8b8b92579c5975
Michael Bonner <mike@bootpackdigital.com>
2025-05-29 22:33:28 -0600

    Run svelte-5 migration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
-<script>
+<script lang="ts">
 	import '../styles/tailwind.css';
+	interface Props {
+		children?: import('svelte').Snippet;
+	}
+
+	let { children }: Props = $props();
 </script>
 
 <div class="ui-main-wrapper flex flex-col items-center justify-center px-4 lg:min-h-screen lg:px-0">
 	<div
 		class="mx-6 my-4 w-full max-w-4xl rounded-lg bg-white px-6 py-4 shadow-lg lg:mx-auto lg:mt-12 lg:px-12 lg:py-8 xl:px-24 xl:py-12"
 	>
-		<slot />
+		{@render children?.()}
 	</div>
 
 	<div class="prose mt-4 mb-8">
